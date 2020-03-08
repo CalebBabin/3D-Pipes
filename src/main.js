cf7de6d5e03f96f7adb65ef5f9f2416189847c84
Caleb <CalebBabin@users.noreply.github.com>
2020-03-08 13:12:44 -0500

    intial commit

---
+++ src/main.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+const Pipe = require('./pipe.js');
+
+window.addEventListener('DOMContentLoaded', () => {
+	let camera, scene, renderer;
+	let pipes = [];
+
+	init();
+	draw();
+
+	function init() {
+		camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
+		camera.position.z = 100;
+		camera.position.x = 100;
+		camera.lookAt(0,0,0);
+
+		scene = new THREE.Scene();
+
+		const light = new THREE.AmbientLight( 0x555555 ); // soft white light
+		scene.add( light );
+
+		const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+		light.position.set( 0, 100, 0.25 );
+		directionalLight.castShadow = true;
+		directionalLight.shadow.mapSize.width = Math.pow(2, 13);  // default
+		directionalLight.shadow.mapSize.height = Math.pow(2, 13); // default
+		directionalLight.shadow.camera.near = -100;
+		directionalLight.shadow.camera.far = 1000;
+
+		const shadowCameraSize = 300;
+		directionalLight.shadow.camera.left = -shadowCameraSize;
+		directionalLight.shadow.camera.bottom = -shadowCameraSize;
+		directionalLight.shadow.camera.top = shadowCameraSize;
+		directionalLight.shadow.camera.right = shadowCameraSize;
+
+
+		scene.add( directionalLight );
+
+		renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.shadowMap.enabled = true;
+		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(renderer.domElement);
+
+		const numberOfPipes = Math.random()*4;
+		for (let index = 0; index < numberOfPipes; index++) {
+			pipes.push(new Pipe(scene));
+		}
+	}
+
+	function draw() {
+		requestAnimationFrame(draw);
+
+		for (let index = 0; index < pipes.length; index++) {
+			const pipe = pipes[index];
+			pipe.tick();
+		}
+
+		renderer.render(scene, camera);
+	}
+})
+
+/*
+setTimeout(()=>{
+	window.location.reload();
+}, 10000);*/
---
+++ src/pipe.js
@@ -0,0 +1,130 @@
+import * as THREE from 'three';
+
+const rand = (max) => {
+	if (typeof max === 'object') {
+		return max[Math.floor(Math.random()*max.length)];
+	} else {
+		return Math.floor(Math.random()*max);
+	}
+}
+const removeTarget = (array, target) => {
+	for (let index = 0; index < array.length; index++) {
+		if (array[index] == target) {
+			array.splice(index, 1);
+		}
+	}
+}
+
+class Pipe {
+	constructor (scene, config = {}) {
+		this.config = Object.assign({
+			color: 0x888888*Math.random()+0x888888,
+			tickDistance: 7,
+			pipeWidth: 2,
+			pipeTickInterval: 4,
+			maximumTicks: 340,
+			maximumDistance: 100,
+		}, config);
+
+		this.scene = scene;
+
+		this.pos = [
+			rand(10)-5, // x
+			rand(10)-5, // y
+			rand(10)-5, // z
+		];
+
+		this.meshes = [];
+		this.activePipe = null;
+		
+		this.ticks = 0;
+		this.realTicks = 0;
+
+		// x+, x-, y+, y-, z+, z-
+		this.direction = rand(6);
+
+		this.cylinder_geometry = new THREE.CylinderBufferGeometry( this.config.pipeWidth, this.config.pipeWidth, this.config.tickDistance, 32 );
+		this.cylinder_material = new THREE.MeshPhongMaterial( {color: this.config.color} );
+		this.cylinder_material.shininess = 100;
+
+		this.sphere_geometry = new THREE.SphereBufferGeometry( this.config.pipeWidth*1.1, 32, 32 );
+		this.sphere_geometry2 = new THREE.SphereBufferGeometry( this.config.pipeWidth*1.25, 32, 32 );
+		
+
+	}
+
+	changeDirection () {
+		// Don't go in the opposite direction
+		// Don't go too far off screen
+
+		let options = [0,1,2,3,4,5];
+
+		for (let index = 0; index < 5; index++) {
+			options.push(this.direction);
+		}
+
+		removeTarget(options, (this.direction % 2 === 0) ? this.direction + 1 : this.direction - 1 );
+
+		for (let index = 0; index < this.pos.length; index++) {
+			// x+, x-, y+, y-, z+, z-
+			if (this.pos[index] >= this.config.maximumDistance) {
+				removeTarget(options, index*2);
+			}
+			if (this.pos[index] <= -this.config.maximumDistance) {
+				removeTarget(options, index*2+1);
+			}
+		}
+
+		this.direction = rand(options);
+	}
+
+	tick () {
+		this.ticks++;
+
+		this.lastPos = [...this.pos];
+		this.lastDirection = this.direction;
+
+		if (this.ticks % this.config.pipeTickInterval == 0) {
+			this.realTicks++;
+			this.changeDirection();
+
+			this.pos[Math.floor(this.direction/2)] += (this.direction % 2 === 0) ? this.config.tickDistance : -this.config.tickDistance;
+
+			if (this.direction === this.lastDirection && this.activePipe) {
+				this.activePipe.scale.y++;
+				this.activePipe.position.z = (this.config.tickDistance/2)*this.activePipe.scale.y;
+			} else {
+				const group = new THREE.Group();
+				this.scene.add(group);
+
+				group.position.x = this.lastPos[0];
+				group.position.y = this.lastPos[1];
+				group.position.z = this.lastPos[2];
+				group.lookAt(this.pos[0], this.pos[1], this.pos[2]);
+
+				const cylinder = new THREE.Mesh( this.cylinder_geometry, this.cylinder_material );
+				cylinder.castShadow = true;
+				cylinder.receiveShadow = true;
+				this.activePipe = cylinder;
+				cylinder.rotation.x += Math.PI/2;
+				cylinder.position.z += this.config.tickDistance/2;
+				group.add(cylinder);
+				
+				const temp_geometry = Math.random() < 0.75 ? this.sphere_geometry : this.sphere_geometry2;
+				const sphere = new THREE.Mesh( (this.direction === this.lastDirection) ? this.sphere_geometry : temp_geometry, this.cylinder_material );
+				sphere.castShadow = true;
+				sphere.receiveShadow = true;
+				sphere.position.x = this.lastPos[0];
+				sphere.position.y = this.lastPos[1];
+				sphere.position.z = this.lastPos[2];
+				this.scene.add( sphere );
+			}
+		}
+
+		if (this.realTicks > this.config.maximumTicks) {
+			window.location.reload();
+		}
+	}
+}
+
+module.exports = Pipe;
